099b1e1fd59a15a1e45ef2c789d6dbac9ca4e99b
Ismael Bernal <ismaelbernal83@gmail.com>
2022-01-23 08:36:05 +0100

    Add frontend endpoint of the reverseproxy in k8s cluster

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://a916b17ad28b0419fb10233414c15e79-1211525053.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
